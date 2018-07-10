e52d7f5ff728c334301d7b2b25400abed2d54af9
Nicole Freed <nicole.freed@gmail.com>
2018-07-10 16:06:02 -0700

    style: create container elements to better control styling

--- js/main.js
+++ js/main.js
@@ -158,27 +158,36 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
+  const imageContainer = document.createElement('div');
+  imageContainer.className = 'restaurant-img-container';
+
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  imageContainer.append(image);
+
+  const infoContainer = document.createElement('div');
+  infoContainer.className = 'restaurant-info';
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  infoContainer.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  infoContainer.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  infoContainer.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  infoContainer.append(more)
+
+  li.append(imageContainer);
+  li.append(infoContainer);
 
   return li
 }
