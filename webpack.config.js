4744f6af24901410462309cc0d80909ec88e7162
Nicole Freed <nicole.freed@gmail.com>
2018-08-03 12:58:02 -0700

    chore: Configure Webpack for clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './client/src/js/main.js',
@@ -8,6 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, './client/dist')
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Restaurant Reviews, Stage 2',
       template: './client/src/index.html',
