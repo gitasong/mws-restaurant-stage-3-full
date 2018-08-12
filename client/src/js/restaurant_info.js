2683a9811e5fd5be25690f3d0d6ad66d0b60e453
Nicole Freed <nicole.freed@gmail.com>
2018-08-12 14:23:08 -0700

    fix: Add self to newMap in fetchRestaurantFromURL so map displays properly

--- client/src/js/restaurant_info.js
+++ client/src/js/restaurant_info.js
@@ -33,7 +33,7 @@ self.initMap = () => {  // initMap() and its methods and properties need to be c
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         id: 'mapbox.streets'
-      }).addTo(newMap);
+      }).addTo(self.newMap);
       fillBreadcrumb();
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
     }
