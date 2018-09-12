4b594da0f1627c0497a1a9f787a9653f6bbcdb66
Nicole Freed <nicole.freed@gmail.com>
2018-09-12 14:05:45 -0700

    refactor: Remove external assets from static cache

--- client/src/sw.js
+++ client/src/sw.js
@@ -9,8 +9,9 @@ const leafletURL = 'https://unpkg.com';
 
 // Open cache; cache site assets
 self.addEventListener('install', (event) => {
+
   event.waitUntil(
-    caches.open('restaurants-v1').then((cache) => {
+    caches.open(staticCacheName).then((cache) => {
       return cache.addAll([
         '/',
         'index.html',
@@ -29,15 +30,15 @@ self.addEventListener('install', (event) => {
         './img/8.jpg',
         './img/9.jpg',
         './img/10.jpg',
-        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-        'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
-        'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon-2x.png',
-        'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
-        'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q',
-        'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1539.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q',
-        'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q',
-        'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1540.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q'
+        // 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        // 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+        // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
+        // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon-2x.png',
+        // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
+        // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q',
+        // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1539.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q',
+        // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q',
+        // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1540.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q'
       ]);
     })
   );
