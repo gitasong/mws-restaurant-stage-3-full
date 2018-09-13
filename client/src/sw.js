fbb38beba7b90f2ba246e4e6329c7f85893e7b44
Nicole Freed <nicole.freed@gmail.com>
2018-09-13 14:18:00 -0700

    fix/refactor: Store leaflet assets locally; update paths

--- client/src/sw.js
+++ client/src/sw.js
@@ -19,7 +19,9 @@ self.addEventListener('install', (event) => {
         'js/main.js',
         'js/restaurant_info.js',
         // 'data/restaurants.json',  // No longer needed because it's being bundled
+        'js/leaflet.js',
         'css/styles.css',
+        'css/leaflet.css',
         './img/1.jpg',
         './img/2.jpg',
         './img/3.jpg',
@@ -30,16 +32,10 @@ self.addEventListener('install', (event) => {
         './img/8.jpg',
         './img/9.jpg',
         './img/10.jpg',
-        // 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-        // 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-        // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
-        // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon-2x.png',
-        // 'https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
-        // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q',
-        // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1539.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q',
-        // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q',
-        // 'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1540.jpg70?access_token=pk.eyJ1IjoibmZyZWVkIiwiYSI6ImNqNnkzc2FrdTFyc2EycW80bzZpcXZrOG8ifQ.3G3y2EHmJYG9TPUEYEK91Q'
-      ]);
+        './img/marker-icon.png',
+        './img/marker-icon-2x.png',
+        './img/marker-shadow.png'
+        ]);
     })
   );
 });
