8a49bfa638d284fc1ac9ebcca74170af70e45764
Nicole Freed <nicole.freed@gmail.com>
2018-11-29 17:08:47 -0800

    refactor: Convert fetchNeighborhoods() to async/await; eliminate callbacks

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -34,15 +34,15 @@ document.addEventListener('DOMContentLoaded', event => {
 /**
  * Fetch all neighborhoods and set their HTML.
  */
-const fetchNeighborhoods = () => {
-  DBHelper.fetchNeighborhoods((error, neighborhoods) => {
-    if (error) { // Got an error
-      console.error(error);
-    } else {
-      self.neighborhoods = neighborhoods;
-      fillNeighborhoodsHTML();
-    }
-  });
+const fetchNeighborhoods = async () => {
+  try {
+    const neighborhoods = await DBHelper.fetchNeighborhoods();
+    self.neighborhoods = neighborhoods;
+    fillNeighborhoodsHTML();
+  }
+  catch(error) {
+    console.log(`Failed to fetch neighborhoods in main.js fetchNeighborhoods() with error ${error}`);
+  }
 }
 
 /**
